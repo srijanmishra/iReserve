bb20f48a256589f42d7f972d0d7ffac19094ade7
gnongsie <gnongsie@visa.com>
2015-09-18 14:22:55 +0530

    ViewMovieBooking code done. Testing remaining.

---
+++ iReserve/Controllers/FoodCourtController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iReserve.Controllers
+{
+    public class FoodCourtController : Controller
+    {
+        //
+        // GET: /FoodCourt/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ iReserve/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using iReserve.Models;
+using GridMvc;
+
+namespace iReserve.Controllers
+{
+    public class MovieController : Controller
+    {
+        //
+        // GET: /Movie/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewMovieBookings()
+        {
+            MovieDAL agent = new MovieDAL();
+            List<ViewMovieBookings> bookingList = agent.bookings();
+
+            return View();
+        }
+
+    }
+}
