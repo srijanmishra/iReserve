e30eaf4370291be93fcf3e7ac68519eaacc06892
gnongsie <gnongsie@visa.com>
2015-09-21 03:25:55 +0530

    Implemented ViewAllBookings.

--- iReserve/Controllers/HomeController.cs
+++ iReserve/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using iReserve.DAL;
+using iReserve.ViewModels;
 
 namespace iReserve.Controllers
 {
@@ -37,6 +39,22 @@ namespace iReserve.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            var userid = Convert.ToInt32(Session["UserID"].ToString());
+            MovieDAL agent1 = new MovieDAL();
+            FoodDAL agent2 = new FoodDAL();
+            PartyDAL agent3 = new PartyDAL();
+            
+            AllBookings bookingCollection = new AllBookings();
+
+            bookingCollection.MovieBookings = agent1.Bookings(10001);
+            bookingCollection.FoodBookings = agent2.MealBookings(10001);
+            bookingCollection.PartyBookings = agent3.Bookings(10001);
+
+            return View(bookingCollection);
         }
     }
 }
---
+++ iReserve/ViewModels/AllBookings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using iReserve.Models;
+
+namespace iReserve.ViewModels
+{
+    public class AllBookings
+    {
+        public List<ViewMovieBookings> MovieBookings { get; set; }
+        public List<ViewMealBookings> FoodBookings { get; set; }
+        public List<ViewPartyBookings> PartyBookings { get; set; }
+    }
+}
